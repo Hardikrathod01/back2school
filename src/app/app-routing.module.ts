347f1f40f7316f4757c4d9c4ad77b9763e0d594f
hardikrathod01 <rathodhardik424@gmail.com>
2020-06-19 23:12:22 +0200

    Added Navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-
-const routes: Routes = [];
+const routes: Routes =[
+  {
+    path: '',
+    component: NavbarComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
